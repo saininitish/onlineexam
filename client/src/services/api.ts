f66e65f9eebb5b8859e646caa6891c16edd5e38a
Nitish kumar <47453102+saininitish@users.noreply.github.com>
2026-04-25 19:29:48 +0530

    url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://onlineexam-1-zhkf.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
