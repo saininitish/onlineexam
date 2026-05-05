899465e9d09c86d7e4bdb86a5ffaa7b4a547356f
Nitish kumar <47453102+saininitish@users.noreply.github.com>
2026-05-05 11:32:26 +0530

    fix

--- client/src/components/admin/LiveProctoring.tsx
+++ client/src/components/admin/LiveProctoring.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useProctoring } from '../../hooks/useProctoring';
 import { useAuthStore } from '../../store/authStore';
 import { motion } from 'framer-motion';
